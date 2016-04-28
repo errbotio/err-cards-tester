1db88f53be372f48670e2856a07ea4769fdc7eda
Guillaume Binet <gbin@google.com>
2016-04-28 09:29:47 -0700

    more cases

--- cards.py
+++ cards.py
@@ -1,21 +1,51 @@
 from errbot import BotPlugin, botcmd
 
-class Card(object):
-    def __init__(self, title='', body=''):
-        self.title=title
-        self.body=body
+LOPSUM = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod " \
+         "tempor incididunt ut labore et dolore magna aliqua."
+
 
 class CardsMaker(BotPlugin):
-    @botcmd(template='card')
+    @botcmd
     def cards(self, msg, args):
-        c = Card('my title', 'my body')
-        yield c.__dict__
-        c = Card('my title', 'my body')
-        c.thumbnail = 'https://raw.githubusercontent.com/errbotio/errbot/master/docs/_static/err.png'
-        yield c.__dict__
-        c = Card('my title', 'my body')
-        c.link = 'http://www.google.com'
-        yield c.__dict__
-        c = Card('my title', 'my body')
-        c.image = 'http://www.rudebaguette.com/assets/big-data.jpg'
-        yield c.__dict__
+        self.send_card(body='Body Only',
+                       in_reply_to=msg)
+        self.send_card(title='Title + Body',
+                       body=LOPSUM,
+                       in_reply_to=msg)
+        self.send_card(title='Title + Body + Thumbnail',
+                       body=LOPSUM,
+                       thumbnail='https://raw.githubusercontent.com/errbotio/errbot/master/docs/_static/err.png',
+                       in_reply_to=msg)
+        self.send_card(title='Title + Body + Link',
+                       body=LOPSUM,
+                       link='http://www.google.com',
+                       in_reply_to=msg)
+        self.send_card(title='Title + Body + Image',
+                       body=LOPSUM,
+                       image='https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png',
+                       in_reply_to=msg)
+        self.send_card(title='Summary + Title + Body',
+                       summary='Important Latin text, read carefully !',
+                       body=LOPSUM,
+                       in_reply_to=msg)
+        self.send_card(title='Title + Body + Fields',
+                       body=LOPSUM,
+                       fields=(('First Key','Value1'), ('Second Key','Value2')),
+                       in_reply_to=msg)
+        self.send_card(title='Title + Body + Color positive',
+                       body=LOPSUM,
+                       color='red',
+                       in_reply_to=msg)
+        self.send_card(title='Title + Body + Color negative',
+                       body=LOPSUM,
+                       color='black',
+                       in_reply_to=msg)
+        self.send_card(title='EVEEERYTHINNNG !!!',
+                       body='text body to put in the card',
+                       thumbnail='https://raw.githubusercontent.com/errbotio/errbot/master/docs/_static/err.png',
+                       image='https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png',
+                       link='http://www.google.com',
+                       fields=(('First Key', 'Value1'), ('Second Key', 'Value2')),
+                       color='red',
+                       in_reply_to=msg)
+        return 'cards sent'
